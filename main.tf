6fdcdcff03a80932e998d248ef390fb09b9a14c3
ronaldo.brisa <familiaberohi@gmail.com>
2025-01-13 22:43:38 -0300

    space correct

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 terraform {
-  
+
 }
 
 provider "aws" {
-  
+
 }
 
 resource "aws_instance" "vm1" {
-  
+
 }
 
 data "aws_ami" "image" {
-  
+
 }
 
 module "network" "source" {
-  
+
 }
 
 variable "vm_name" {
-  
+
 }
 
-output "vm1_ip" "value"{
-  
+output "vm1_ip" "value" {
+
 }
 
 locals {
-  
+
 }
