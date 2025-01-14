43645aee59f649e245435fa625daf4920bf34a6e
ronaldo.brisa <familiaberohi@gmail.com>
2025-01-13 23:28:53 -0300

    config terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 terraform {
+  required_version = "1.10.3"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.0.0"
+    }
+
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-1234567890"
+
+  }
 }
 
 provider "aws" {
